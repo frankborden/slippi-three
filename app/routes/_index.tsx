c2419cbc5006042700d3db8965f1630f7262eb6a
Frank Borden <frankborden2@gmail.com>
2024-02-19 18:12:51 +0100

    wip

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,52 +7,48 @@ export default function Index() {
     <div className="grid h-screen place-items-center bg-slate-100">
       <div className="aspect-square size-[500px] rounded border border-slate-300 bg-white drop-shadow">
         <Canvas>
-          <Render />
-          <ambientLight intensity={Math.PI} />
-          <spotLight
-            position={[10, 10, 10]}
-            angle={0.15}
-            penumbra={1}
-            decay={0}
-            intensity={Math.PI}
-          />
-          <pointLight
-            position={[-10, -10, -10]}
-            decay={0}
-            intensity={Math.PI}
-          />
-          <Box position={[-1.2, 0, 0]} />
-          <Box position={[1.2, 0, 0]} />
+          <Replay />
         </Canvas>
       </div>
     </div>
   );
 }
 
-function Render() {
-  const { rotation, setRotation } = useRotationStore();
+function Replay() {
+  const { rotation, setRotation } = store();
   useFrame(() => {
     setRotation([rotation[0] + 0.01, rotation[1] + 0.04, rotation[2] + 0.01]);
-  }, 1);
-  useFrame(({ gl, scene, camera }) => {
-    gl.render(scene, camera);
-  }, 3);
-  return null;
+  }, -1);
+
+  return (
+    <>
+      <ambientLight intensity={Math.PI} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        decay={0}
+        intensity={Math.PI}
+      />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </>
+  );
 }
 
 function Box(props: any) {
   const meshRef = useRef<JSX.IntrinsicElements["mesh"] | null>(null);
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
-  useFrame(
-    (state, delta) =>
-      meshRef.current!.rotation!.set(
-        useRotationStore.getState().rotation[0],
-        useRotationStore.getState().rotation[1],
-        useRotationStore.getState().rotation[2],
-      ),
-    2,
-  );
+  useFrame(() => {
+    const storeRotation = store.getState().rotation;
+    meshRef.current!.rotation!.set(
+      storeRotation[0],
+      storeRotation[1],
+      storeRotation[2],
+    );
+  });
 
   return (
     <mesh
@@ -69,12 +65,12 @@ function Box(props: any) {
   );
 }
 
-interface RotationStore {
+interface Store {
   rotation: [number, number, number];
   setRotation: (rotation: [number, number, number]) => void;
 }
 
-const useRotationStore = create<RotationStore>((set) => ({
+const store = create<Store>((set) => ({
   rotation: [0, 0, 0],
   setRotation: (rotation: [number, number, number]) => set({ rotation }),
 }));
