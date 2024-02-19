8489227f452f4934d8926e2f99710d82b85fba6f
Frank Borden <frankborden2@gmail.com>
2024-02-19 17:30:01 +0100

    wip

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,31 +1,59 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useRef, useState } from "react";
+import { create } from "zustand";
 
 export default function Index() {
   return (
-    <div>
-      <div className="text-9xl">crstneia</div>
-      <Canvas>
-      <ambientLight intensity={Math.PI / 2} />
-      <spotLight
-        position={[10, 10, 10]}
-        angle={0.15}
-        penumbra={1}
-        decay={0}
-        intensity={Math.PI}
-      />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-    </Canvas></div>
+    <div className="grid h-screen place-items-center bg-slate-100">
+      <div className="aspect-square size-[500px] rounded border border-slate-300 bg-white drop-shadow">
+        <Canvas>
+          <Render />
+          <ambientLight intensity={Math.PI} />
+          <spotLight
+            position={[10, 10, 10]}
+            angle={0.15}
+            penumbra={1}
+            decay={0}
+            intensity={Math.PI}
+          />
+          <pointLight
+            position={[-10, -10, -10]}
+            decay={0}
+            intensity={Math.PI}
+          />
+          <Box position={[-1.2, 0, 0]} />
+          <Box position={[1.2, 0, 0]} />
+        </Canvas>
+      </div>
+    </div>
   );
 }
 
+function Render() {
+  const { rotation, setRotation } = useRotationStore();
+  useFrame(() => {
+    setRotation([rotation[0] + 0.01, rotation[1] + 0.04, rotation[2] + 0.01]);
+  }, 1);
+  useFrame(({ gl, scene, camera }) => {
+    gl.render(scene, camera);
+  }, 3);
+  return null;
+}
+
 function Box(props: any) {
-  const meshRef = useRef<(JSX.IntrinsicElements['mesh']) | null>(null);
+  const meshRef = useRef<JSX.IntrinsicElements["mesh"] | null>(null);
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
-  useFrame((state, delta) => (meshRef.current!.rotation!.x += delta));
+  useFrame(
+    (state, delta) =>
+      meshRef.current!.rotation!.set(
+        useRotationStore.getState().rotation[0],
+        useRotationStore.getState().rotation[1],
+        useRotationStore.getState().rotation[2],
+      ),
+    2,
+  );
+
   return (
     <mesh
       {...props}
@@ -40,3 +68,13 @@ function Box(props: any) {
     </mesh>
   );
 }
+
+interface RotationStore {
+  rotation: [number, number, number];
+  setRotation: (rotation: [number, number, number]) => void;
+}
+
+const useRotationStore = create<RotationStore>((set) => ({
+  rotation: [0, 0, 0],
+  setRotation: (rotation: [number, number, number]) => set({ rotation }),
+}));
--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="@remix-run/cloudflare" />
 /// <reference types="vite/client" />
-
 import type { KVNamespace } from "@cloudflare/workers-types";
 
 declare module "@remix-run/cloudflare" {
