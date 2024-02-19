6167e7ff6b34409a217fa26f5592fd0328d99256
Frank Borden <frankborden2@gmail.com>
2024-02-19 21:07:05 +0100

    changing animations

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,8 +6,7 @@ import {
 } from "@react-three/drei";
 import { Canvas, GroupProps, useFrame } from "@react-three/fiber";
 import { decode } from "@shelacek/ubjson";
-import { useEffect, useMemo, useRef } from "react";
-import { SkeletonUtils } from "three-stdlib";
+import { useRef } from "react";
 import { create } from "zustand";
 
 import { parseReplay } from "~/parser";
@@ -37,10 +36,8 @@ export default function Index() {
 }
 
 function Replay() {
-  const { position, setPosition } = store();
-  useFrame(() => {
-    setPosition([(position[0] + 0.1) % 20, position[1] + 0, position[2] + 0]);
-  }, -1);
+  const { frame, setFrame } = store();
+  useFrame(() => setFrame(frame + 1), -1);
 
   return (
     <>
@@ -56,7 +53,6 @@ function Replay() {
         position={[0, 0, 100]}
         zoom={1}
       />
-      <Character rotation={[Math.PI / 2, 0, 0]} />
       <Character rotation={[0, Math.PI / 2, 0]} />
     </>
   );
@@ -64,31 +60,29 @@ function Replay() {
 
 function Character(props: GroupProps) {
   const { scene, animations } = useGLTF("/models/fox.glb");
-  const clone = useMemo(() => SkeletonUtils.clone(scene), [scene]);
   const ref = useRef<JSX.IntrinsicElements["group"] | null>(null);
-  const { actions } = useAnimations(animations, clone);
+  const { mixer, actions } = useAnimations(animations, scene);
 
   useFrame(() => {
-    ref.current!.position!.set(
-      store.getState().position[0],
-      store.getState().position[1],
-      store.getState().position[2],
-    );
+    if (store.getState().frame % 400 === 1) {
+      mixer.stopAllAction();
+      actions.AttackAirB!.play();
+    }
+    if (store.getState().frame % 400 === 201) {
+      mixer.stopAllAction();
+      actions.AttackAirF!.play();
+    }
   });
 
-  useEffect(() => {
-    actions.AttackAirB.play();
-  }, [actions]);
-
-  return <primitive {...props} object={clone} ref={ref} dispose={null} />;
+  return <primitive {...props} object={scene} ref={ref} dispose={null} />;
 }
 
 interface Store {
-  position: [number, number, number];
-  setPosition: (position: [number, number, number]) => void;
+  frame: number;
+  setFrame: (frame: number) => void;
 }
 
 const store = create<Store>((set) => ({
-  position: [0, 0, 0],
-  setPosition: (position: [number, number, number]) => set({ position }),
+  frame: 0,
+  setFrame: (frame: number) => set({ frame }),
 }));
