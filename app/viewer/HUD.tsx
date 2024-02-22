410cadcff2eae0d4d11c4ad79095e646d1ff2821
Frank Borden <frankborden2@gmail.com>
2024-02-22 23:48:18 +0100

    Refactor HUD component and add timer

--- app/viewer/HUD.tsx
+++ app/viewer/HUD.tsx
@@ -3,8 +3,11 @@ import { store } from "~/viewer/store";
 export function HUD() {
   const { replay, frame } = store();
   return (
-    <div className="absolute bottom-1 left-0 w-full text-white">
-      <div className="flex justify-around">
+    <>
+      <div className="absolute left-0 top-1 flex w-full justify-center text-white">
+        {timer(frame)}
+      </div>
+      <div className="absolute bottom-1 left-0 flex w-full justify-around text-white">
         {replay?.settings.playerSettings.map((settings, i) => (
           <div
             key={i}
@@ -26,13 +29,88 @@ export function HUD() {
                 />
               ))}
             </div>
-            <div>
+            <div className="text-lg font-bold">
               {Math.round(replay.frames[frame].players[i].state.percent)}%
             </div>
-            <div>{settings.connectCode}</div>
+            <div className="text-sm">{settings.connectCode}</div>
           </div>
         ))}
       </div>
-    </div>
+    </>
   );
+}
+
+const meleeHundredths = [
+  "00",
+  "02",
+  "04",
+  "06",
+  "07",
+  "09",
+  "11",
+  "12",
+  "14",
+  "16",
+  "17",
+  "19",
+  "21",
+  "22",
+  "24",
+  "26",
+  "27",
+  "29",
+  "31",
+  "32",
+  "34",
+  "36",
+  "37",
+  "39",
+  "41",
+  "42",
+  "44",
+  "46",
+  "47",
+  "49",
+  "51",
+  "53",
+  "54",
+  "56",
+  "58",
+  "59",
+  "61",
+  "63",
+  "64",
+  "66",
+  "68",
+  "69",
+  "71",
+  "73",
+  "74",
+  "76",
+  "78",
+  "79",
+  "81",
+  "83",
+  "84",
+  "86",
+  "88",
+  "89",
+  "91",
+  "93",
+  "94",
+  "96",
+  "98",
+  "99",
+];
+
+function timer(frame: number) {
+  const frames = Math.min(60 * 60 * 8, 60 * 60 * 8 - frame + 123);
+  const minutes = Math.floor(frames / (60 * 60))
+    .toString()
+    .padStart(2, "0");
+  const seconds = Math.floor((frames % (60 * 60)) / 60)
+    .toString()
+    .padStart(2, "0");
+  const hundredths = meleeHundredths[frames % 60];
+  return `${minutes}:${seconds}:${hundredths}`;
 }
