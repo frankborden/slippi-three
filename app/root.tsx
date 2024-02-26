fdd262c63adf8061d0ae8d32ceb207a66b76f745
Frank Borden <frankborden2@gmail.com>
2024-02-27 00:30:25 +0100

    wip tabs

--- app/root.tsx
+++ app/root.tsx
@@ -6,7 +6,9 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useNavigate,
 } from "@remix-run/react";
+import { RouterProvider } from "react-aria-components";
 import { twMerge as cn } from "tailwind-merge";
 
 import "~/root.css";
@@ -16,6 +18,8 @@ if (globalThis.navigator && "serviceWorker" in globalThis.navigator) {
 }
 
 export default function App() {
+  let navigate = useNavigate();
+
   return (
     <html lang="en">
       <head>
@@ -25,32 +29,34 @@ export default function App() {
         <Links />
       </head>
       <body className="bg-neutral-950 text-neutral-200">
-        <div className="flex h-screen gap-4">
-          <nav className="flex flex-col items-center gap-8 rounded-lg bg-neutral-900 p-6">
-            <Link
-              className="mb-4 grid size-10 place-items-center rounded bg-neutral-200 font-bold leading-none text-neutral-950"
-              to="/"
-            >
-              SL
-            </Link>
-            <PageLink icon="i-tabler-device-tv-old" to="/replays" />
-            <PageLink icon="i-tabler-notebook" to="/framedata" />
-            <PageLink icon="i-tabler-angle" to="/calculator" />
-            <a
-              href="https://github.com/frankborden/slippilab"
-              target="_blank"
-              className="i-tabler-brand-github mt-auto text-3xl text-neutral-400 hover:text-neutral-300"
-            />
-            <a
-              href="https://twitter.com/slippilab"
-              target="_blank"
-              className="i-tabler-brand-twitter text-3xl text-neutral-400 hover:text-neutral-300"
-            />
-            <PageLink icon="i-tabler-user-circle" to="account" />
-            <PageLink icon="i-tabler-settings" to="/settings" />
-          </nav>
-          <Outlet />
-        </div>
+        <RouterProvider navigate={navigate}>
+          <div className="flex h-screen">
+            <nav className="flex flex-col items-center gap-8 rounded-lg border-r border-r-neutral-700 bg-neutral-900 p-6">
+              <Link
+                className="mb-4 grid size-10 place-items-center rounded bg-neutral-200 font-bold leading-none text-neutral-950"
+                to="/"
+              >
+                SL
+              </Link>
+              <PageLink icon="i-tabler-device-tv-old" to="/replays" />
+              <PageLink icon="i-tabler-notebook" to="/framedata" />
+              <PageLink icon="i-tabler-angle" to="/calculator" />
+              <a
+                href="https://github.com/frankborden/slippilab"
+                target="_blank"
+                className="i-tabler-brand-github mt-auto text-3xl text-neutral-400 hover:text-neutral-300"
+              />
+              <a
+                href="https://twitter.com/slippilab"
+                target="_blank"
+                className="i-tabler-brand-twitter text-3xl text-neutral-400 hover:text-neutral-300"
+              />
+              <PageLink icon="i-tabler-user-circle" to="account" />
+              <PageLink icon="i-tabler-settings" to="/settings" />
+            </nav>
+            <Outlet />
+          </div>
+        </RouterProvider>
         <ScrollRestoration />
         <Scripts />
       </body>
--- app/routes/replays.tsx
+++ app/routes/replays.tsx
@@ -1,51 +1,71 @@
-import { decode } from "@shelacek/ubjson";
-import { Button, FileTrigger } from "react-aria-components";
+// import { decode } from "@shelacek/ubjson";
+// import { Button, FileTrigger } from "react-aria-components";
+import { Tab, TabList, TabPanel, Tabs } from "react-aria-components";
 
-import { parseReplay } from "~/parser";
-import { Controls } from "~/viewer/Controls";
-import { Replay } from "~/viewer/Replay";
-import { renderReplay } from "~/viewer/render";
-import { store } from "~/viewer/store";
+// import { parseReplay } from "~/parser";
+// import { Controls } from "~/viewer/Controls";
+// import { Replay } from "~/viewer/Replay";
+// import { renderReplay } from "~/viewer/render";
+// import { store } from "~/viewer/store";
 
-export default function Index() {
-  const {
-    setRenderData,
-    setReplay,
-    setFrame,
-    setPaused,
-    openedTimestamp,
-    setOpenedTimestamp,
-    replay,
-  } = store();
+// export default function Index() {
+//   const {
+//     setRenderData,
+//     setReplay,
+//     setFrame,
+//     setPaused,
+//     openedTimestamp,
+//     setOpenedTimestamp,
+//     replay,
+//   } = store();
 
-  async function openFile(files: FileList | null) {
-    if (!files) return;
-    const { raw, metadata } = decode(await files[0].arrayBuffer(), {
-      useTypedArrays: true,
-    });
-    const replay = parseReplay(metadata, raw);
-    setReplay(replay);
-    setRenderData(renderReplay(replay));
-    setFrame(0);
-    setPaused(false);
-    setOpenedTimestamp(Date.now());
-  }
+//   async function openFile(files: FileList | null) {
+//     if (!files) return;
+//     const { raw, metadata } = decode(await files[0].arrayBuffer(), {
+//       useTypedArrays: true,
+//     });
+//     const replay = parseReplay(metadata, raw);
+//     setReplay(replay);
+//     setRenderData(renderReplay(replay));
+//     setFrame(0);
+//     setPaused(false);
+//     setOpenedTimestamp(Date.now());
+//   }
 
+//   return (
+//     <main className="flex h-screen flex-col gap-4 overflow-y-auto p-8">
+//       <FileTrigger onSelect={openFile}>
+//         <Button className="w-max rounded bg-emerald-700 px-3 py-0.5 text-emerald-50 hover:bg-emerald-600 hover:text-white">
+//           Open Replay
+//         </Button>
+//       </FileTrigger>
+//       {replay && (
+//         <>
+//           <div className="relative flex shrink flex-col overflow-y-auto rounded border border-neutral-700 bg-neutral-950">
+//             <Replay key={openedTimestamp} />
+//           </div>
+//           <Controls />
+//         </>
+//       )}
+//     </main>
+//   );
+// }
+
+export default function Page() {
+  return <ReplayList />;
+}
+
+function ReplayList() {
   return (
-    <main className="flex h-screen flex-col gap-4 overflow-y-auto p-8">
-      <FileTrigger onSelect={openFile}>
-        <Button className="w-max rounded bg-emerald-700 px-3 py-0.5 text-emerald-50 hover:bg-emerald-600 hover:text-white">
-          Open Replay
-        </Button>
-      </FileTrigger>
-      {replay && (
-        <>
-          <div className="relative flex shrink flex-col overflow-y-auto rounded border border-neutral-700 bg-neutral-950">
-            <Replay key={openedTimestamp} />
-          </div>
-          <Controls />
-        </>
-      )}
-    </main>
+    <Tabs>
+      <TabList className="flex gap-2 p-4 *:rounded *:px-2 *:outline-none *:transition-colors *:duration-200 [&>[data-selected]]:bg-neutral-200 [&>[data-selected]]:text-neutral-950">
+        <Tab id="uploads">Uploads</Tab>
+        <Tab id="events">Events</Tab>
+        <Tab id="personal">Personal</Tab>
+      </TabList>
+      <TabPanel id="uploads">uploads</TabPanel>
+      <TabPanel id="events">events</TabPanel>
+      <TabPanel id="personal">personal</TabPanel>
+    </Tabs>
   );
 }
