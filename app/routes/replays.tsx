f516d0b1e464b8f558ae5d1968e7d916ae30305a
Frank Borden <frankborden2@gmail.com>
2024-03-02 01:08:28 +0100

    split replay list component

--- app/routes/replays.tsx
+++ app/routes/replays.tsx
@@ -2,7 +2,6 @@ import { decode } from "@shelacek/ubjson";
 import {
   Button,
   FileTrigger,
-  Label,
   ListBox,
   ListBoxItem,
   Tab,
@@ -22,12 +21,18 @@ import { Controls } from "~/viewer/Controls";
 import { Replay } from "~/viewer/Replay";
 import { renderReplay } from "~/viewer/render";
 
+const pageSize = 10;
+
 export default function Page() {
   const { openedTimestamp, replay } = store();
 
   return (
     <div className="flex grow">
-      <ReplayList />
+      <div className="border-r border-r-zinc-700 bg-zinc-900 px-4 py-2">
+        <Sources />
+        <Filters />
+        <Replays />
+      </div>
       <div className="grow bg-zinc-800 p-4">
         {replay && (
           <>
@@ -42,23 +47,110 @@ export default function Page() {
   );
 }
 
-function ReplayList() {
+function Sources() {
+  const { addFiles, setCurrentSource, setCurrentPage, currentSource } = store();
+
+  return (
+    <Tabs
+      onSelectionChange={(key) => {
+        setCurrentSource(key as any);
+        setCurrentPage(0);
+      }}
+      selectedKey={currentSource}
+    >
+      <TabList className="mb-2 flex w-max gap-3 rounded-lg border border-zinc-600 p-1 text-sm *:rounded *:px-2 *:outline-none *:transition-colors *:duration-200 [&>[data-hovered]]:cursor-pointer [&>[data-hovered]]:bg-zinc-700 [&>[data-hovered]]:text-zinc-100 [&>[data-selected]]:bg-zinc-300 [&>[data-selected]]:text-zinc-950">
+        <Tab id="personal">Personal</Tab>
+        <Tab id="uploads">Uploads</Tab>
+        <Tab id="events">Events</Tab>
+      </TabList>
+      <TabPanel id="personal">
+        <div className="mb-2 flex gap-4 text-sm">
+          <FileTrigger
+            onSelect={(files: FileList | null) => {
+              if (!files) return;
+              addFiles(Array.from(files));
+            }}
+            acceptedFileTypes={[".slp"]}
+            allowsMultiple
+          >
+            <Button className="rounded bg-zinc-300 px-2 text-zinc-950 hover:bg-zinc-100 hover:text-black">
+              Open Files
+            </Button>
+          </FileTrigger>
+          <FileTrigger
+            acceptDirectory
+            onSelect={(files: FileList | null) => {
+              if (!files) return;
+              addFiles(Array.from(files));
+            }}
+          >
+            <Button className="rounded bg-zinc-300 px-3 py-0.5 text-zinc-950 hover:bg-zinc-100 hover:text-black">
+              Open Folder
+            </Button>
+          </FileTrigger>
+        </div>
+      </TabPanel>
+      <TabPanel id="uploads">uploads</TabPanel>
+      <TabPanel id="events">events</TabPanel>
+    </Tabs>
+  );
+}
+
+function Filters() {
+  const { filters, setFilters, setCurrentPage } = store();
+
+  return (
+    <TagGroup
+      className="mb-2 flex items-center gap-2"
+      onRemove={(key) => {
+        setFilters(filters.filter((_, i) => !key.has(String(i))));
+        setCurrentPage(0);
+      }}
+    >
+      <TagList
+        items={filters.map((f, i) => [f, i] as const)}
+        className="flex flex-wrap items-center gap-2"
+      >
+        {([filter, i]) => (
+          <Tag
+            id={String(i)}
+            textValue={String(
+              filter.type === "character"
+                ? shortCharactersExt[filter.externalCharacterId]
+                : filter.type === "stage"
+                  ? stages[filter.stageId]
+                  : "crst",
+            )}
+            className="flex items-center gap-1 rounded border px-1 text-sm transition-colors duration-200 has-[button:hover]:bg-zinc-700 focus:outline-none"
+          >
+            <div>
+              {filter.type === "character"
+                ? shortCharactersExt[filter.externalCharacterId]
+                : filter.type === "stage"
+                  ? stages[filter.stageId]
+                  : "crst"}
+            </div>
+            <Button slot="remove" className="i-tabler-x" />
+          </Tag>
+        )}
+      </TagList>
+    </TagGroup>
+  );
+}
+
+function Replays() {
   const {
     setRenderData,
     setReplay,
     setFrame,
     setPaused,
     setOpenedTimestamp,
-    addFiles,
     localStubs,
     currentPage,
-    setCurrentPage,
-    selectedStub,
     setSelectedStub,
-    currentSource,
-    setCurrentSource,
+    selectedStub,
     filters,
-    setFilters,
+    setCurrentPage,
   } = store();
 
   const stubs = localStubs;
@@ -83,83 +175,9 @@ function ReplayList() {
     }
     return true;
   });
-  const pageSize = 10;
-
-  async function openFile(files: FileList | null) {
-    if (!files) return;
-    addFiles(Array.from(files));
-  }
 
   return (
-    <div className="border-r border-r-zinc-700 bg-zinc-900 px-4 py-2">
-      <Tabs
-        onSelectionChange={(key) => {
-          setCurrentSource(key as any);
-          setCurrentPage(0);
-        }}
-        selectedKey={currentSource}
-      >
-        <TabList className="mb-2 flex w-max gap-3 rounded-lg border border-zinc-600 p-1 text-sm *:rounded *:px-2 *:outline-none *:transition-colors *:duration-200 [&>[data-hovered]]:cursor-pointer [&>[data-hovered]]:bg-zinc-700 [&>[data-hovered]]:text-zinc-100 [&>[data-selected]]:bg-zinc-300 [&>[data-selected]]:text-zinc-950">
-          <Tab id="personal">Personal</Tab>
-          <Tab id="uploads">Uploads</Tab>
-          <Tab id="events">Events</Tab>
-        </TabList>
-        <TabPanel id="personal">
-          <div className="mb-2 flex gap-4 text-sm">
-            <FileTrigger
-              onSelect={openFile}
-              acceptedFileTypes={[".slp"]}
-              allowsMultiple
-            >
-              <Button className="rounded bg-zinc-300 px-2 text-zinc-950 hover:bg-zinc-100 hover:text-black">
-                Open Files
-              </Button>
-            </FileTrigger>
-            <FileTrigger acceptDirectory onSelect={openFile}>
-              <Button className="rounded bg-zinc-300 px-3 py-0.5 text-zinc-950 hover:bg-zinc-100 hover:text-black">
-                Open Folder
-              </Button>
-            </FileTrigger>
-          </div>
-        </TabPanel>
-        <TabPanel id="uploads">uploads</TabPanel>
-        <TabPanel id="events">events</TabPanel>
-      </Tabs>
-      <TagGroup
-        className="mb-2 flex items-center gap-2"
-        onRemove={(key) => {
-          setFilters(filters.filter((_, i) => !key.has(String(i))));
-          setCurrentPage(0);
-        }}
-      >
-        <TagList
-          items={filters.map((f, i) => [f, i] as const)}
-          className="flex flex-wrap items-center gap-2"
-        >
-          {([filter, i]) => (
-            <Tag
-              id={String(i)}
-              textValue={String(
-                filter.type === "character"
-                  ? shortCharactersExt[filter.externalCharacterId]
-                  : filter.type === "stage"
-                    ? stages[filter.stageId]
-                    : "crst",
-              )}
-              className="flex items-center gap-1 rounded border px-1 text-sm transition-colors duration-200 has-[button:hover]:bg-zinc-700 focus:outline-none"
-            >
-              <div>
-                {filter.type === "character"
-                  ? shortCharactersExt[filter.externalCharacterId]
-                  : filter.type === "stage"
-                    ? stages[filter.stageId]
-                    : "crst"}
-              </div>
-              <Button slot="remove" className="i-tabler-x" />
-            </Tag>
-          )}
-        </TagList>
-      </TagGroup>
+    <>
       <ListBox
         items={filteredStubs.slice(
           currentPage * pageSize,
@@ -280,6 +298,6 @@ function ReplayList() {
           className="i-tabler-chevron-right-pipe text-xl disabled:text-zinc-400"
         />
       </div>
-    </div>
+    </>
   );
 }
